7140d784f3d66b0ea20a1c96e9a589b494cbf476
mdecourse <mdecourse@gmail.com>
2018-08-27 21:19:51 +0800

    update blog title

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'KMOL'
-SITENAME = '機械設計工程系 <br>2018 Fall 分組網誌'
+SITENAME = '機械設計工程系 2018 Fall 分組網誌'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
